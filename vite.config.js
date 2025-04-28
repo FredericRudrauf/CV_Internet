5e44fc8ad3370beefeb53247c030edae6f39f8a9
Frederic Rudrauf <136611508+FredericRudrauf@users.noreply.github.com>
2025-04-27 23:15:50 -0400

    Update vite.config.js
    Correction pour publier sur GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/CV_Internet/', // Ajoutez cette ligne pour spécifier le chemin de base
 })
